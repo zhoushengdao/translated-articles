b681808ab56d6b339337b2546b05f912e005b22e
zhoushengdao <zhoushengdao@sina.cn>
2023-08-23 23:44:06 +0800

    make bcd compile better

--- origin-private-file-system/compile.mjs
+++ origin-private-file-system/compile.mjs
@@ -1,10 +1,10 @@
-import { watch, writeFileSync } from "node:fs";
+import { watch, writeFileSync, readFileSync } from "node:fs";
 
 import pug from "pug";
+import { JSDOM } from "jsdom";
+import { PurgeCSS } from "purgecss";
 
-// pnpx purgecss
-
-const path = "./bcd.pug";
+const path = "bcd.pug";
 
 const locals = {
   check: (key, value) => {
@@ -20,6 +20,24 @@ const locals = {
     desktop: "桌面",
     mobile: "移动",
     server: "服务器",
+  },
+  browsers: {
+    chrome: { icon: "chrome", text: "Chrome" },
+    edge: { icon: "edge", text: "Edge" },
+    firefox: { icon: "simple-firefox", text: "Firefox" },
+    opera: { icon: "opera", text: "Opera" },
+    safari: { icon: "safari", text: "Safari" },
+    chrome_android: { icon: "chrome", text: "Chrome Android" },
+    firefox_android: { icon: "simple-firefox", text: "Firefox for Android" },
+    opera_android: { icon: "opera", text: "Opera Android" },
+    safari_ios: { icon: "safari", text: "Safari on iOS" },
+    samsunginternet_android: {
+      icon: "samsunginternet",
+      text: "Samsung Internet",
+    },
+    webview_android: { icon: "webview_android", text: "WebView Android" },
+    deno: { icon: "deno", text: "Deno" },
+    nodejs: { icon: "nodejs", text: "Node.js" },
   },
   supportStatus: {
     yes: "完全支持",
@@ -38,10 +56,29 @@ const locals = {
   },
 };
 
-const compileFile = () => {
-  writeFileSync(`index.html`, pug.compileFile(path, { self: true })(locals));
-  console.log(`\x1B[36m${path}\x1B[0m \x1B[2mhas updated\x1B[0m`);
+const compileFile = async () => {
+  const output = { html: "index.html", css: "main.css" };
+
+  writeFileSync(output.html, pug.compileFile(path, { self: true })(locals));
+  console.log(`\x1B[36m${output.html}\x1B[0m \x1B[2mhas updated\x1B[0m`);
+
+  const {
+    window: { document },
+  } = new JSDOM(readFileSync(output.html, { encoding: "utf-8" }));
+  const allClasses = [].concat(
+    ...[...document.querySelectorAll("*")].map((el) => [...el.classList]),
+  );
+  const [{ css: minCss }] = await new PurgeCSS().purge({
+    content: ["index.html"],
+    css: ["main.b9154aa3.css"],
+    safelist: [...new Set(allClasses)],
+  });
+  writeFileSync(
+    output.css,
+    minCss.replaceAll("/static/", "https://developer.mozilla.org/static/"),
+  );
+  console.log(`\x1B[36m${output.css}\x1B[0m \x1B[2mhas updated\x1B[0m`);
 };
 
-compileFile();
-watch(path, () => compileFile());
+await compileFile();
+watch(path, async () => await compileFile());
